47c79c19f31a664f8f4e6f2692dc1fe19dbfe49b
warley lima <warley.info@gmail.com>
2018-06-22 11:23:04 -0300

    Porta

--- AuditoriaApi/Program.cs
+++ AuditoriaApi/Program.cs
@@ -19,6 +19,7 @@ namespace AuditoriaApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("https://localhost:5052");
     }
 }
